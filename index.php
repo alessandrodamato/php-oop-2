b510824f454b880fafa8b4f25273e1e48aa57138
alessandrodamato <alessandro042004@gmail.com>
2024-05-03 17:50:38 +0200

    [add] partials

--- index.php
+++ index.php
@@ -4,19 +4,8 @@ require_once __DIR__ . '/Models/Category.php';
 require_once __DIR__ . '/Models/Subcategory.php';
 require_once __DIR__ . '/Models/Product.php';
 require_once __DIR__ . '/assets/data/db.php';
-
+require_once __DIR__ . '/partials/head.php'
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/style/style.css">
-  <title>PHP OOP 2</title>
-</head>
 
 <body>
 
