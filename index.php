3e5af38af0c61078f55f7a9d5307838596786d6c
alessandrodamato <alessandro042004@gmail.com>
2024-05-03 17:40:44 +0200

    [add] styles

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/Models/Category.php';
 require_once __DIR__ . '/Models/Subcategory.php';
 require_once __DIR__ . '/Models/Product.php';
 require_once __DIR__ . '/assets/data/db.php';
 
-
-var_dump($db)
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,31 +17,73 @@ var_dump($db)
   <link rel="stylesheet" href="assets/style/style.css">
   <title>PHP OOP 2</title>
 </head>
+
 <body>
 
-  <!-- <div class="container">
+  <header>
+    <div class="header-top"></div>
+    <div class="header-bottom d-flex align-items-center justify-content-center">
+      <h1><a class="text-black text-decoration-none fw-bold" href="#">Boonimal</a></h1>
+    </div>
+  </header>
 
-    <div class="row">
+  <main class="pt-5">
 
+    <div class="container">
 
-      <?php foreach($db as $product): ?>
-        <div class="col">
+      <div class="row">
 
-          <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-          
+        <div class="col-4">
+          <h4 class="mb-4">Filtri:</h4>
+          <ul class="list-unstyled">
+            <li class="cp">Categoria</li>
+            <li class="cp">Sottocategoria</li>
+          </ul>
         </div>
-      <?php endforeach ?>
+
+        <div class="col-8">
+
+          <div class="container">
+
+            <div class="row row-cols-3">
+
+              <?php foreach ($db as $product) : ?>
+                <div class="col d-flex justify-content-center">
+
+                  <div class="card mb-5" style="width: 18rem;">
+                    <img src="<?php echo $product->image ?>" class="card-img-top w-100 h-100 object-fit-contain " alt="<?php echo $product->name ?>">
+                    <div class="card-body">
+                      <h4 class="card-title"><?php echo $product->name ?></h4>
+                      <p class="card-text"><?php echo $product->description ?></p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item bg-body-secondary"><strong>Category: </strong><?php echo $product->animal ?></li>
+                      <li class="list-group-item bg-body-secondary"><strong>Type: </strong><?php echo $product->type ?></li>
+                      <li class="list-group-item bg-body-secondary"><strong>Price: </strong>&euro; <?php echo $product->price ?></li>
+                    </ul>
+                  </div>
+
+                </div>
+              <?php endforeach ?>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
 
     </div>
 
-  </div> -->
+  </main>
+
+  <footer class="d-flex align-items-center justify-content-center text-white">
+    
+    <h4>Boonimal &copy;</h4>
+
+  </footer>
 
 </body>
+
 </html>
